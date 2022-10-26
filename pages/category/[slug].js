a8fdf33665922a33b922fc2d435c3ecb9ca1c0af
unknown <erfan8202@gmail.com>
2022-10-26 15:25:08 +0330

    add title

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,18 +1,16 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import React from "react";
+import Head from "next/head";
 
-import { getCategories, getCategoryPost } from '../../services';
-import { PostCard, Categories, Loader } from '../../components';
+import { getCategories, getCategoryPost } from "../../services";
+import { PostCard, Categories } from "../../components";
 
 const CategoryPost = ({ posts }) => {
-  const router = useRouter();
-
-  if (router.isFallback) {
-    return <Loader />;
-  }
-
   return (
     <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Categories | React-Blog</title>
+        <link rel="icon" href="/icons8-react-100.png" />
+      </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post, index) => (
@@ -39,8 +37,6 @@ export async function getStaticProps({ params }) {
   };
 }
 
-// Specify dynamic routes to pre-render pages based on data.
-// The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
   const categories = await getCategories();
   return {
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,6 +1,6 @@
 import React from "react";
 import { getPosts, getPostDetails } from "../../services";
-import { useRouter } from "next/dist/client/router";
+import Head from "next/head";
 import {
   PostDetail,
   Categories,
@@ -13,6 +13,10 @@ import {
 const PostDetails = ({ post }) => {
   return (
     <div className="container mx-auto pb-10 mb-8">
+      <Head>
+        <title>{post.slug} | React-Blog</title>
+        <link rel="icon" href="/icons8-react-100.png" />
+      </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           <PostDetail post={post} />
@@ -49,6 +53,6 @@ export async function getStaticPaths() {
 
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: true
+    fallback: true,
   };
 }
