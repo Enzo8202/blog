5daa92cdd2a61efc8934c37a9632f2934922a4cd
unknown <erfan8202@gmail.com>
2022-10-26 22:23:35 +0330

    loading added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,28 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Layout } from "../components";
+import Router from 'next/router';
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
